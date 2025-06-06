f862b087b65940f6978094779dd7409d098086ab
Capondecap <bunhour38@gmail.com>
2025-06-06 15:21:36 +0700

    Merge master with main

--- urlfind.py
+++ urlfind.py
@@ -2,13 +2,18 @@ import urllib.request
 from bs4 import BeautifulSoup
 import ssl
 
-
-
 inp = input('Enter your link: ')
+
+#ignore ssl cerificate (not recommended)
 context = ssl._create_unverified_context()
 
+#open read link
 url =  urllib.request.urlopen(inp, context=context).read()
+
+#parse data 
 soup = BeautifulSoup(url,'html.parser')
+
+#find href tag
 anchor = soup("a")
 for i in anchor:
     print(i.get("href"))
